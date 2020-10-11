5f827545b4f30519fd44da2b0c09426943d44cc1
Swetalina Dash <swetalinadash@Swetalinas-Air.hsd1.ca.comcast.net>
2020-10-11 14:46:55 -0700

    pushchange

--- index.ts
+++ index.ts
@@ -2,9 +2,6 @@ import express from 'express';
 // import the sagger lib
 import swaggerUi = require('swagger-ui-express');
 import bodyParser = require("body-parser");
-import fs = require('fs')
-import { parse } from 'path';
-const path = require('path');
 const app = express()
 
       
@@ -35,11 +32,32 @@ app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.post('/api/v1/parse', (req, res) => {
    let parseData = req.body.data;
    if(parseData && parseData.length>0 && parseData.indexOf("0")>-1){
-    parseData.split("0");
+    let splitData = parseData.split("0");
     responseObj.statusCode=200;
-    responseObj.data.firstName=parseData[0];
-    responseObj.data.lastName=parseData[1];
-    responseObj.data.clientId=parseData[2];
+    let firstName=splitData[0];
+    let lastName="";
+    let count=0;
+    let appendFirst="0";
+    let appendLast="0";
+  for(let i=1;i<splitData.length;i++){
+    if(splitData[i].length>0){   
+      if(count==0){
+        lastName=splitData[i];
+        count=1;
+      }else{
+        responseObj.data.clientId=splitData[i];
+      }
+      
+    }else{
+      if(count==0){
+        appendFirst+="0"
+      }else if(count==1){
+        appendLast+="0";
+      }
+    }
+  }
+  responseObj.data.firstName=firstName+appendFirst;
+  responseObj.data.lastName=lastName+appendLast;
     res.json(responseObj);
    }else{
     errorObj.statusCode=500;
@@ -53,12 +71,26 @@ app.post('/api/v1/parse', (req, res) => {
 app.post('/api/v2/parse', (req, res) => {
   let parseData = req.body.data;
   if(parseData && parseData.length>0 && parseData.indexOf("0")>-1){
-   parseData.replace("^0*", "-")
-   parseData.split("-")
-   responseObj.statusCode=200;
-   responseObj.data.firstName=parseData[0];
-    responseObj.data.lastName=parseData[1];
-    responseObj.data.clientId=parseData[2];
+  let splitData = parseData.split("0");
+  responseObj.statusCode=200;
+  responseObj.data.firstName=splitData[0];
+  let count=0;
+  for(let i=1;i<splitData.length;i++){
+    if(splitData[i].length>0){   
+      if(count==0){
+        responseObj.data.lastName=splitData[i];
+        count=1;
+      }else{
+       
+        let objStr=splitData[i].substring(0,3)+"-"+splitData[i].substring(3,splitData[i].length)
+        responseObj.data.clientId=objStr;
+      }
+      
+    }
+  }
+    
+
+    
   }else{
     errorObj.statusCode=500;
     errorObj.msg="Error while parsing data" ;
